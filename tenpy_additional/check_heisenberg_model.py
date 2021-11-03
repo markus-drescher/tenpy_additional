4745427728adc90fe8cb5914a5c91055e762300b
Markus Drescher <65250562+markus-drescher@users.noreply.github.com>
2021-11-03 17:29:11 +0100

    check bond dimensions of Hamiltonians in check_heisenberg_model

---
+++ tenpy_additional/check_heisenberg_model.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from tenpy_additional.heisenberg_triangular_model import heisenberg_triangular
+from tenpy_additional.heisenberg_triangular_model import heisenberg_triangular_NN
+
+
+L = 6
+Lx = 2
+J1xy = 1.
+J1z = 1.
+J2xy = 0.125
+J2z = 0.125
+bc = 'infinite'
+conserve = 'Sz'
+
+
+model_params = dict(Lx=Lx, Ly=L,
+                    J1xy = J1xy, J1z = J1z,
+                    J2xy = J2xy, J2z = J2z,
+                    bc_MPS = bc, conserve = conserve,
+                    lattice = 'Triangular',
+                    bc_y = 'cylinder',
+                    )
+
+M = heisenberg_triangular(model_params)
+print(M.H_MPO.chi)
+
+model_params2 = dict(Lx=Lx, Ly=L,
+                    J1xy = J1xy, J1z = J1z,
+                    bc_MPS = bc, conserve = conserve,
+                    lattice = 'Triangular',
+                    bc_y = 'cylinder',
+                    )
+
+M2 = heisenberg_triangular_NN(model_params2)
+print(M2.H_MPO.chi)
