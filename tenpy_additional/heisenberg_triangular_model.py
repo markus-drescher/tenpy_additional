e6a997e757fac9414f24baf01287b62a3dadbc9f
Markus Drescher <65250562+markus-drescher@users.noreply.github.com>
2021-11-03 17:12:43 +0100

    added heisenberg_triangular_NN in heisenberg_triangular_model

--- tenpy_additional/heisenberg_triangular_model.py
+++ tenpy_additional/heisenberg_triangular_model.py
@@ -52,4 +52,36 @@ class heisenberg_triangular(CouplingMPOModel):
             print('u1, u2, dx', u1, u2, dx)
 
 
+class heisenberg_triangular_NN(CouplingMPOModel):
+    r"""Spin-1/2 sites coupled by next-nearest neighbour interactions
+
+    The Hamiltonian reads:
+
+    .. math ::
+        H = \sum_{\langle i, j \rangle, i < j}
+               2 \cdot \mathtt{J^{(1)}_{xy}} [S^+_i S^-_j + S^-_i S^+_j]
+               + 4 \cdot mathtt{J^{(1)}_{z}} S^z_i S^z_j
+
+    """
+    def init_sites(self, model_params):
+        S = 0.5
+        conserve = model_params.get('conserve', 'best')
+        if conserve == 'best':
+            conserve = 'Sz'
+
+        site = SpinSite(S, conserve)
+        return site
+
+    def init_terms(self, model_params):
+        # read out/set default parameters
+        J1xy = model_params.get('J1xy', 1.)
+        J1z = model_params.get('J1z', 1.)
+
+        print('*'*80)
+        print('Nearest neighbour pairs')
+        for u1, u2, dx in self.lat.pairs['nearest_neighbors']:
+            self.add_coupling(2*J1xy, u1, 'Sp', u2, 'Sm', dx, plus_hc=True)
+            self.add_coupling(4*J1z, u1, 'Sz', u2, 'Sz', dx, plus_hc=False)
+            print('u1, u2, dx', u1, u2, dx)
+
 
